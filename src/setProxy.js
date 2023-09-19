0cfd09bdcc589797d2d2058c5cd40d0865a249bf
siwon <muncool39@gmail.com>
2023-09-19 16:34:11 +0900

    proxy setting

--- src/setProxy.js
+++ src/setProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/", {
-      target: "http://localhost:8080",
+      target: "http://http://ec2-44-198-225-181.compute-1.amazonaws.com:8080",
       changeOrigin: true,
     })
   );
